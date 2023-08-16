01e021e04a9fb934a981cc96be1b337f5ab9a591
Thad House <thadhouse1@gmail.com>
2023-08-15 21:42:06 -0700

    Add PIO IRQ

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,12 @@
 //! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
-#![deny(warnings)]
 
 use bsp::{entry, hal};
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::{digital::v2::OutputPin, prelude::*};
-use hal::prelude::_rphal_pio_PIOExt;
+use embedded_hal::{digital::v2::OutputPin, prelude::*, spi::MODE_3};
+use hal::{prelude::_rphal_pio_PIOExt, spi};
 use panic_probe as _;
 
 mod spi_pio;
@@ -27,6 +26,8 @@ use bsp::hal::{
     sio::Sio,
     watchdog::Watchdog,
 };
+
+use crate::spi_pio::{spi_pio_init_0, Pio0Cfg};
 
 #[entry]
 fn main() -> ! {
@@ -50,9 +51,9 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let mut _delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-    let pins = bsp::Pins::new(
+    let pins: bsp::Pins = bsp::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
@@ -62,7 +63,7 @@ fn main() -> ! {
     let sda_pin = pins.gpio16.into_mode::<bsp::hal::gpio::FunctionI2C>();
     let scl_pin = pins.gpio17.into_mode::<bsp::hal::gpio::FunctionI2C>();
 
-    let mut i2c = hal::I2C::i2c0(
+    let mut _i2c = hal::I2C::i2c0(
         pac.I2C0,
         sda_pin,
         scl_pin,
@@ -71,38 +72,78 @@ fn main() -> ! {
         &clocks.system_clock,
     );
 
-    let pio = pac.PIO0.split(&mut pac.RESETS);
-    let ms = pio.1;
+    let cfg = Pio0Cfg {
+        pio: pac.PIO0,
+        cs_pin: pins.gpio13,
+        cipo_pin: pins.gpio10,
+        copi_pin: pins.gpio11,
+        sck_pin: pins.gpio12,
+    };
 
-    spi_pio_init();
+    spi_pio_init_0(cfg, &mut pac.RESETS);
 
-    let write_bytes: [u8; 1] = [0x07];
-    let mut read_bytes: [u8; 1] = [0];
-
-    match i2c.write_read(0x52, &write_bytes, &mut read_bytes) {
-        Ok(()) => {
-            info!("Success!");
-        }
-        Err(e) => {
-            defmt::info!("Error {}", e);
-        }
-    }
-
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead. If you have
-    // a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here.
-    let mut led_pin = pins.gpio13.into_push_pull_output();
+    // spi_pio_init(pac.PIO0, &mut pac.RESETS);
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        cortex_m::asm::wfi();
     }
+
+    // // These are implicitly used by the spi driver if they are in the correct mode
+    // let _spi_mosi = pins.gpio12.into_mode::<bsp::hal::gpio::FunctionSpi>();
+    // let _spi_miso = pins.gpio11.into_mode::<bsp::hal::gpio::FunctionSpi>();
+    // let _spi_sclk = pins.gpio10.into_mode::<bsp::hal::gpio::FunctionSpi>();
+    // let spi = spi::Spi::<_, _, 8>::new(pac.SPI1);
+
+    // let mut cs = pins
+    //     .gpio13
+    //     .into_push_pull_output_in_state(hal::gpio::PinState::High);
+
+    // let mut spi = spi.init(
+    //     &mut pac.RESETS,
+    //     clocks.peripheral_clock.freq(),
+    //     1.MHz(),
+    //     &MODE_3,
+    // );
+
+    // loop {
+    //     cs.set_low();
+    //     spi.write(&[1, 2, 3, 4]).unwrap();
+    //     cs.set_high();
+    //     _delay.delay_ms(100);
+    // }
+
+    // //let pio = pac.PIO0.split(&mut pac.RESETS);
+    // //let ms: hal::pio::UninitStateMachine<(pac::PIO0, hal::pio::SM0)> = pio.1;
+
+    // //spi_pio_init();
+
+    // let write_bytes: [u8; 1] = [0x07];
+    // let mut read_bytes: [u8; 1] = [0];
+
+    // match i2c.write_read(0x52, &write_bytes, &mut read_bytes) {
+    //     Ok(()) => {
+    //         info!("Success!");
+    //     }
+    //     Err(e) => {
+    //         defmt::info!("Error {}", e);
+    //     }
+    // }
+
+    // // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
+    // // on-board LED, it might need to be changed.
+    // // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead. If you have
+    // // a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
+    // // LED to one of the GPIO pins, and reference that pin here.
+    // let mut led_pin = pins.gpio13.into_push_pull_output();
+
+    // loop {
+    //     info!("on!");
+    //     led_pin.set_high().unwrap();
+    //     delay.delay_ms(500);
+    //     info!("off!");
+    //     led_pin.set_low().unwrap();
+    //     delay.delay_ms(500);
+    // }
 }
 
 // End of file
--- src/spi_pio.rs
+++ src/spi_pio.rs
@@ -1,16 +1,37 @@
+use core::cell::RefCell;
+
+use critical_section::Mutex;
 use pio::Instruction;
+use rp2040_hal::gpio::{AnyPin, Disabled, Pin, PinId, PullDown};
+use rp2040_hal::pac::interrupt;
+use rp2040_hal::pac::interrupt::PIO0_IRQ_0;
+use rp2040_hal::pio::StateMachine;
 use rp2040_hal::{
     gpio::{FunctionPio0, Pins},
     pac::{self, PIO0},
     pio::{PIOBuilder, PIOExt, PinDir},
 };
 
-const CS_PIN: u8 = 1;
-const CIPO_PIN: u8 = 2;
-const COPI_PIN: u8 = 3;
+pub struct Pio0Cfg<CS: PinId, CIPO: PinId, COPI: PinId, SCK: PinId> {
+    pub pio: PIO0,
+    pub cs_pin: Pin<CS, Disabled<PullDown>>,
+    pub cipo_pin: Pin<CIPO, Disabled<PullDown>>,
+    pub copi_pin: Pin<COPI, Disabled<PullDown>>,
+    pub sck_pin: Pin<SCK, Disabled<PullDown>>,
+}
 
-pub fn spi_pio_init(pio: PIO0, pins: &mut Pins, resets: &mut pac::RESETS) {
-    let (mut pio, sm0, sm1, _, _) = pio.split(resets);
+struct GlobalPio0 {
+    pio: rp2040_hal::pio::PIO<PIO0>,
+    cs_sm: StateMachine<(PIO0, rp2040_hal::pio::SM0), rp2040_hal::pio::Running>,
+}
+
+static GLOBAL_PIO_0: Mutex<RefCell<Option<GlobalPio0>>> = Mutex::new(RefCell::new(None));
+
+pub fn spi_pio_init_0<CS: PinId, CIPO: PinId, COPI: PinId, SCK: PinId>(
+    cfg: Pio0Cfg<CS, CIPO, COPI, SCK>,
+    resets: &mut pac::RESETS,
+) {
+    let (mut pio, sm0, sm1, _, _) = cfg.pio.split(resets);
 
     let cs_program = pio_proc::pio_asm!(
         ".side_set 1 pindirs",
@@ -21,64 +42,107 @@ pub fn spi_pio_init(pio: PIO0, pins: &mut Pins, resets: &mut pac::RESETS) {
         "irq set 2 side 0",    //trigger rising edge irq",
     );
 
+    cfg.cs_pin.id();
+
     let cs_installed = pio.install(&cs_program.program).unwrap();
-    let (mut cs_sm, cs_rx, cs_tx) = PIOBuilder::from_program(cs_installed)
-        .in_pin_base(CS_PIN)
-        .side_set_pin_base(CIPO_PIN)
+    let (mut cs_sm, _, _) = PIOBuilder::from_program(cs_installed)
+        .in_pin_base(cfg.cs_pin.id().num)
+        .side_set_pin_base(cfg.cipo_pin.id().num)
         .build(sm0);
 
-    cs_sm.set_pindirs([(CIPO_PIN, PinDir::Input)]);
+    cs_sm.set_pindirs([(cfg.cipo_pin.id().num, PinDir::Input)]);
 
-    let program = pio_proc::pio_asm!(
-        "wait_falling:",
-        "wait 0 pin 1", // Wait for falling clock edge
-        "initial_loop:",
-        "pull ifempty noblock", // Pull if there is data
-        "out pins 1",           // Write CIPO
-        "wait 1 pin 1",         // Wait for rising clock edge
-        "in pins 1",            // Read COPI
-        "push iffull noblock",  // Push if we've read 8 bits, but don't block if fifo is full
-        ".wrap",
-        ".wrap_target",
-        "jmp wait_falling",
-        "public initial_check:",
-        "jmp y-- wait_falling",
-        "irq set 0", // Trigger data set irq
-        "jmp wait_falling",
-    );
+    let irq0 = pio.irq0();
+    irq0.enable_sm_interrupt(1);
+    irq0.enable_sm_interrupt(2);
 
-    let installed = pio.install(&program.program).unwrap();
-    let (mut bits_sm, bits_rx, bits_tx) = PIOBuilder::from_program(installed)
-        .in_pin_base(COPI_PIN)
-        .out_pins(CIPO_PIN, 1)
-        .out_shift_direction(rp2040_hal::pio::ShiftDirection::Left)
-        .in_shift_direction(rp2040_hal::pio::ShiftDirection::Left)
-        .autopull(false)
-        .autopush(false)
-        .push_threshold(8)
-        .pull_threshold(8)
-        .build(sm1);
-    let cipo = pins.gpio2.into_mode::<FunctionPio0>();
-    assert_eq!(cipo.id().num, CIPO_PIN);
+    let cs_sm = cs_sm.start();
 
-    bits_tx.write(0xFF << 24);
-    bits_sm.exec_instruction(Instruction {
-        operands: pio::InstructionOperands::PULL {
-            if_empty: false,
-            block: true,
-        },
-        delay: 0,
-        side_set: None,
+    critical_section::with(|crit| {
+        GLOBAL_PIO_0
+            .borrow(crit)
+            .replace(Some(GlobalPio0 { pio, cs_sm }))
     });
-    bits_sm.exec_instruction(Instruction {
-        operands: pio::InstructionOperands::MOV {
-            destination: pio::MovDestination::X,
-            op: pio::MovOperation::None,
-            source: pio::MovSource::OSR,
-        },
-        delay: 0,
-        side_set: None,
-    });
+
+    unsafe {
+        pac::NVIC::unmask(PIO0_IRQ_0);
+    }
+
+    // let program = pio_proc::pio_asm!(
+    //     "wait_falling:",
+    //     "wait 0 pin 1", // Wait for falling clock edge
+    //     "initial_loop:",
+    //     "pull ifempty noblock", // Pull if there is data
+    //     "out pins 1",           // Write CIPO
+    //     "wait 1 pin 1",         // Wait for rising clock edge
+    //     "in pins 1",            // Read COPI
+    //     "push iffull noblock",  // Push if we've read 8 bits, but don't block if fifo is full
+    //     ".wrap",
+    //     ".wrap_target",
+    //     "jmp wait_falling",
+    //     "public initial_check:",
+    //     "jmp y-- wait_falling",
+    //     "irq set 0", // Trigger data set irq
+    //     "jmp wait_falling",
+    // );
+
+    // let installed = pio.install(&program.program).unwrap();
+    // let (mut bits_sm, bits_rx, bits_tx) = PIOBuilder::from_program(installed)
+    //     .in_pin_base(COPI_PIN)
+    //     .out_pins(CIPO_PIN, 1)
+    //     .out_shift_direction(rp2040_hal::pio::ShiftDirection::Left)
+    //     .in_shift_direction(rp2040_hal::pio::ShiftDirection::Left)
+    //     .autopull(false)
+    //     .autopush(false)
+    //     .push_threshold(8)
+    //     .pull_threshold(8)
+    //     .build(sm1);
+    // let cipo = pins.gpio2.into_mode::<FunctionPio0>();
+    // assert_eq!(cipo.id().num, CIPO_PIN);
+
+    // bits_tx.write(0xFF << 24);
+    // bits_sm.exec_instruction(Instruction {
+    //     operands: pio::InstructionOperands::PULL {
+    //         if_empty: false,
+    //         block: true,
+    //     },
+    //     delay: 0,
+    //     side_set: None,
+    // });
+    // bits_sm.exec_instruction(Instruction {
+    //     operands: pio::InstructionOperands::MOV {
+    //         destination: pio::MovDestination::X,
+    //         op: pio::MovOperation::None,
+    //         source: pio::MovSource::OSR,
+    //     },
+    //     delay: 0,
+    //     side_set: None,
+    // });
 }
 
-pub fn wait_for_data() {}
+fn stop_loops(pio: &mut GlobalPio0) {
+    pio.cs_sm.restart();
+}
+
+fn prepare_for_next(pio: &mut GlobalPio0) {}
+
+#[interrupt]
+fn PIO0_IRQ_0() {
+    static mut PIO: Option<GlobalPio0> = None;
+
+    if PIO.is_none() {
+        critical_section::with(|crit| *PIO = GLOBAL_PIO_0.borrow(crit).take());
+    }
+
+    if let Some(pio) = PIO {
+        let irqs = pio.pio.get_irq_raw();
+        if (irqs & (1u8 << 1)) != 0 {
+            defmt::info!("Falling!");
+            pio.pio.clear_irq(1u8 << 1);
+        }
+        if (irqs & (1u8 << 2)) != 0 {
+            defmt::info!("Rising!");
+            pio.pio.clear_irq(1u8 << 2);
+        }
+    }
+}

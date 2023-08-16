163d0403c79817eb2a0af9222bedddee7fb69871
Thad House <thadhouse1@gmail.com>
2023-08-15 23:02:38 -0700

    Get SPI side mostly working

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,37 @@
 //! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
+#![deny(unsafe_code)]
+#![deny(warnings)]
 
-use bsp::{entry, hal};
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::{digital::v2::OutputPin, prelude::*, spi::MODE_3};
-use hal::{prelude::_rphal_pio_PIOExt, spi};
+use embedded_hal::digital::v2::InputPin;
+use hal::{
+    entry,
+    gpio::{bank0::Gpio13, FunctionI2c, FunctionSio, Pin, PullUp, SioInput},
+};
 use panic_probe as _;
+use rp2040_hal as hal;
 
 mod spi_pio;
 
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use rp_pico as bsp;
 // use sparkfun_pro_micro_rp2040 as bsp;
 
 use fugit::RateExtU32;
 
-use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac,
-    sio::Sio,
-    watchdog::Watchdog,
-};
+use hal::{clocks::Clock, pac, watchdog::Watchdog};
 
 use crate::spi_pio::{spi_pio_init_0, Pio0Cfg};
+
+#[allow(unsafe_code)]
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
+
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 
 #[entry]
 fn main() -> ! {
@@ -35,12 +41,10 @@ fn main() -> ! {
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
 
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
+    // Configure the clocks
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -53,15 +57,18 @@ fn main() -> ! {
 
     let mut _delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-    let pins: bsp::Pins = bsp::Pins::new(
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+
+    let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
 
-    let sda_pin = pins.gpio16.into_mode::<bsp::hal::gpio::FunctionI2C>();
-    let scl_pin = pins.gpio17.into_mode::<bsp::hal::gpio::FunctionI2C>();
+    let sda_pin = pins.gpio16.into_function::<FunctionI2c>();
+    let scl_pin = pins.gpio17.into_function::<FunctionI2c>();
 
     let mut _i2c = hal::I2C::i2c0(
         pac.I2C0,
@@ -72,20 +79,77 @@ fn main() -> ! {
         &clocks.system_clock,
     );
 
+    let cs_pin: Pin<Gpio13, FunctionSio<SioInput>, PullUp> = pins.gpio13.into_pull_up_input();
+
     let cfg = Pio0Cfg {
         pio: pac.PIO0,
-        cs_pin: pins.gpio13,
+        cs_pin: &cs_pin,
         cipo_pin: pins.gpio10,
         copi_pin: pins.gpio11,
         sck_pin: pins.gpio12,
     };
 
-    spi_pio_init_0(cfg, &mut pac.RESETS);
+    let mut pios = spi_pio_init_0(cfg, &mut pac.RESETS);
 
     // spi_pio_init(pac.PIO0, &mut pac.RESETS);
 
+    let mut read_data = [0u8; 256];
+    let mut write_data = [0u8; 256];
+
     loop {
-        cortex_m::asm::wfi();
+        // Wait for CS to go high
+        while cs_pin.is_low().unwrap() {}
+        pios.bits_sm.restart();
+        pios.bits_sm.clear_fifos();
+        // TODO Clear RX buffer
+        // Wait for CS to go low
+        while cs_pin.is_high().unwrap() {}
+
+        defmt::info!("Went High");
+
+        let mut idx: u8 = 0;
+        while cs_pin.is_low().unwrap() {
+            match pios.bits_rx.read() {
+                Some(d) => {
+                    read_data[idx as usize] = d as u8;
+                    idx = idx.wrapping_add(1);
+                }
+                None => {}
+            }
+        }
+        defmt::info!("Read {} {}", idx, read_data[..idx as usize]);
+
+        // Do I2C Stuff
+        write_data[0] = 8;
+        write_data[1] = 9;
+        write_data[2] = 10;
+        write_data[3] = 11;
+        idx = 0;
+
+        // Set data ready
+
+        // Wait for high
+        while cs_pin.is_low().unwrap() {}
+        pios.bits_sm.restart();
+        pios.bits_sm.clear_fifos();
+
+        // TODO Clear TX Buffer
+
+        // Load buffer
+        pios.bits_tx.write_u8_replicated(write_data[idx as usize]);
+        idx += 1;
+
+        // Wait for CS to go low
+        while cs_pin.is_high().unwrap() {}
+
+        while cs_pin.is_low().unwrap() {
+            // Add more bytes, increment if successfull
+            if pios.bits_tx.write_u8_replicated(write_data[idx as usize]) {
+                idx = idx.wrapping_add(1);
+            }
+        }
+
+        defmt::info!("Write {} {}", idx, write_data[..idx as usize]);
     }
 
     // // These are implicitly used by the spi driver if they are in the correct mode
--- src/spi_pio.rs
+++ src/spi_pio.rs
@@ -1,48 +1,47 @@
-use core::cell::RefCell;
-
-use critical_section::Mutex;
 use pio::Instruction;
-use rp2040_hal::gpio::{AnyPin, Disabled, Pin, PinId, PullDown};
-use rp2040_hal::pac::interrupt;
-use rp2040_hal::pac::interrupt::PIO0_IRQ_0;
-use rp2040_hal::pio::StateMachine;
+use rp2040_hal::gpio::{
+    FunctionNull, FunctionSio, Pin, PinId, PullDown, PullUp, SioInput, ValidFunction,
+};
+use rp2040_hal::pio::{Running, Rx, StateMachine, Tx, PIO, SM0, SM1};
 use rp2040_hal::{
-    gpio::{FunctionPio0, Pins},
+    gpio::FunctionPio0,
     pac::{self, PIO0},
     pio::{PIOBuilder, PIOExt, PinDir},
 };
 
-pub struct Pio0Cfg<CS: PinId, CIPO: PinId, COPI: PinId, SCK: PinId> {
+pub struct Pio0Cfg<'a, CS: PinId, CIPO: PinId, COPI: PinId, SCK: PinId> {
     pub pio: PIO0,
-    pub cs_pin: Pin<CS, Disabled<PullDown>>,
-    pub cipo_pin: Pin<CIPO, Disabled<PullDown>>,
-    pub copi_pin: Pin<COPI, Disabled<PullDown>>,
-    pub sck_pin: Pin<SCK, Disabled<PullDown>>,
+    pub cs_pin: &'a Pin<CS, FunctionSio<SioInput>, PullUp>,
+    pub cipo_pin: Pin<CIPO, FunctionNull, PullDown>,
+    pub copi_pin: Pin<COPI, FunctionNull, PullDown>,
+    pub sck_pin: Pin<SCK, FunctionNull, PullDown>,
 }
 
-struct GlobalPio0 {
-    pio: rp2040_hal::pio::PIO<PIO0>,
-    cs_sm: StateMachine<(PIO0, rp2040_hal::pio::SM0), rp2040_hal::pio::Running>,
+pub struct SPIPio {
+    pub cs_sm: StateMachine<(PIO0, SM0), Running>,
+    pub bits_sm: StateMachine<(PIO0, SM1), Running>,
+    pub bits_tx: Tx<(PIO0, SM1)>,
+    pub bits_rx: Rx<(PIO0, SM1)>,
+    pub pio: PIO<PIO0>,
 }
 
-static GLOBAL_PIO_0: Mutex<RefCell<Option<GlobalPio0>>> = Mutex::new(RefCell::new(None));
-
-pub fn spi_pio_init_0<CS: PinId, CIPO: PinId, COPI: PinId, SCK: PinId>(
+pub fn spi_pio_init_0<
+    CS: PinId,
+    CIPO: PinId + ValidFunction<FunctionPio0>,
+    COPI: PinId,
+    SCK: PinId,
+>(
     cfg: Pio0Cfg<CS, CIPO, COPI, SCK>,
     resets: &mut pac::RESETS,
-) {
+) -> SPIPio {
     let (mut pio, sm0, sm1, _, _) = cfg.pio.split(resets);
 
     let cs_program = pio_proc::pio_asm!(
         ".side_set 1 pindirs",
         "wait 0 pin 0 side 0", // wait for falling edge of cs pin
-        "irq set 7 side 1",
-        "irq set 1 side 1",    // trigger falling edge irq",
-        "wait 1 pin 0 side 1", // wait for rising edge of cs pin",
-        "irq set 2 side 0",    //trigger rising edge irq",
+        "irq clear 7 side 1",  // set loop irq
+        "wait 1 pin 0 side 1", // wait for rising edge of cs pin
     );
-
-    cfg.cs_pin.id();
 
     let cs_installed = pio.install(&cs_program.program).unwrap();
     let (mut cs_sm, _, _) = PIOBuilder::from_program(cs_installed)
@@ -52,97 +51,59 @@ pub fn spi_pio_init_0<CS: PinId, CIPO: PinId, COPI: PinId, SCK: PinId>(
 
     cs_sm.set_pindirs([(cfg.cipo_pin.id().num, PinDir::Input)]);
 
-    let irq0 = pio.irq0();
-    irq0.enable_sm_interrupt(1);
-    irq0.enable_sm_interrupt(2);
-
     let cs_sm = cs_sm.start();
 
-    critical_section::with(|crit| {
-        GLOBAL_PIO_0
-            .borrow(crit)
-            .replace(Some(GlobalPio0 { pio, cs_sm }))
+    let program = pio_proc::pio_asm!(
+        "irq wait 7",
+        "loop:",
+        "wait 0 pin 1",         // Wait for falling clock edge
+        "pull ifempty noblock", // Pull if there is data
+        "out pins 1",           // Write CIPO
+        "wait 1 pin 1",         // Wait for rising clock edge
+        "in pins 1",            // Read COPI
+        "push iffull noblock",  // Push if we've read 8 bits, but don't block if fifo is full
+        "jmp loop"
+    );
+
+    let installed = pio.install(&program.program).unwrap();
+    let (mut bits_sm, bits_rx, mut bits_tx) = PIOBuilder::from_program(installed)
+        .in_pin_base(cfg.copi_pin.id().num)
+        .out_pins(cfg.cipo_pin.id().num, 1)
+        .out_shift_direction(rp2040_hal::pio::ShiftDirection::Left)
+        .in_shift_direction(rp2040_hal::pio::ShiftDirection::Left)
+        .autopull(false)
+        .autopush(false)
+        .push_threshold(8)
+        .pull_threshold(8)
+        .build(sm1);
+    cfg.cipo_pin.into_function::<FunctionPio0>();
+
+    bits_tx.write(0xFF << 24);
+    bits_sm.exec_instruction(Instruction {
+        operands: pio::InstructionOperands::PULL {
+            if_empty: false,
+            block: true,
+        },
+        delay: 0,
+        side_set: None,
+    });
+    bits_sm.exec_instruction(Instruction {
+        operands: pio::InstructionOperands::MOV {
+            destination: pio::MovDestination::X,
+            op: pio::MovOperation::None,
+            source: pio::MovSource::OSR,
+        },
+        delay: 0,
+        side_set: None,
     });
 
-    unsafe {
-        pac::NVIC::unmask(PIO0_IRQ_0);
-    }
+    let bits_sm = bits_sm.start();
 
-    // let program = pio_proc::pio_asm!(
-    //     "wait_falling:",
-    //     "wait 0 pin 1", // Wait for falling clock edge
-    //     "initial_loop:",
-    //     "pull ifempty noblock", // Pull if there is data
-    //     "out pins 1",           // Write CIPO
-    //     "wait 1 pin 1",         // Wait for rising clock edge
-    //     "in pins 1",            // Read COPI
-    //     "push iffull noblock",  // Push if we've read 8 bits, but don't block if fifo is full
-    //     ".wrap",
-    //     ".wrap_target",
-    //     "jmp wait_falling",
-    //     "public initial_check:",
-    //     "jmp y-- wait_falling",
-    //     "irq set 0", // Trigger data set irq
-    //     "jmp wait_falling",
-    // );
-
-    // let installed = pio.install(&program.program).unwrap();
-    // let (mut bits_sm, bits_rx, bits_tx) = PIOBuilder::from_program(installed)
-    //     .in_pin_base(COPI_PIN)
-    //     .out_pins(CIPO_PIN, 1)
-    //     .out_shift_direction(rp2040_hal::pio::ShiftDirection::Left)
-    //     .in_shift_direction(rp2040_hal::pio::ShiftDirection::Left)
-    //     .autopull(false)
-    //     .autopush(false)
-    //     .push_threshold(8)
-    //     .pull_threshold(8)
-    //     .build(sm1);
-    // let cipo = pins.gpio2.into_mode::<FunctionPio0>();
-    // assert_eq!(cipo.id().num, CIPO_PIN);
-
-    // bits_tx.write(0xFF << 24);
-    // bits_sm.exec_instruction(Instruction {
-    //     operands: pio::InstructionOperands::PULL {
-    //         if_empty: false,
-    //         block: true,
-    //     },
-    //     delay: 0,
-    //     side_set: None,
-    // });
-    // bits_sm.exec_instruction(Instruction {
-    //     operands: pio::InstructionOperands::MOV {
-    //         destination: pio::MovDestination::X,
-    //         op: pio::MovOperation::None,
-    //         source: pio::MovSource::OSR,
-    //     },
-    //     delay: 0,
-    //     side_set: None,
-    // });
-}
-
-fn stop_loops(pio: &mut GlobalPio0) {
-    pio.cs_sm.restart();
-}
-
-fn prepare_for_next(pio: &mut GlobalPio0) {}
-
-#[interrupt]
-fn PIO0_IRQ_0() {
-    static mut PIO: Option<GlobalPio0> = None;
-
-    if PIO.is_none() {
-        critical_section::with(|crit| *PIO = GLOBAL_PIO_0.borrow(crit).take());
-    }
-
-    if let Some(pio) = PIO {
-        let irqs = pio.pio.get_irq_raw();
-        if (irqs & (1u8 << 1)) != 0 {
-            defmt::info!("Falling!");
-            pio.pio.clear_irq(1u8 << 1);
-        }
-        if (irqs & (1u8 << 2)) != 0 {
-            defmt::info!("Rising!");
-            pio.pio.clear_irq(1u8 << 2);
-        }
-    }
+    return SPIPio {
+        cs_sm,
+        bits_sm,
+        bits_tx,
+        bits_rx,
+        pio,
+    };
 }
